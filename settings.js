65a9732fb25633163f06a6089ecd9b1cbd8b4463
dollarwowhub-dotcom <dollarwowhub@gmail.com>
2025-10-19 08:39:03 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'dollar',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "dollar",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '923404469538', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
